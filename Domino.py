c8c322e7a5b41c4c881622019ed31b211b94e178
Mesratya <48388826+Mesratya@users.noreply.github.com>
2019-03-12 23:26:45 +0100

    Add files via upload

--- Domino.py
+++ Domino.py
@@ -42,10 +42,11 @@ class talon(list):
 
 
 class hand(list):
-    def __init__(self,num,game):
+    def __init__(self,num,game,mode):
         self.num = num
         self.game = game
         self.etat_bloque = False # si la main ne peut definitivement plus jouer cette variable vaut true
+        self.mode = mode # mode est la nature du joueur : human, IA (il peut y avoir plusieur type d'IA)
 
     def domino_jouable(self):
         '''Renvoie les dominos de la main qui sont posables sur le plateau'''
@@ -84,11 +85,26 @@ class hand(list):
     def max_domino(self):
         """Renvoie le plus grand domino de la main"""
         return(max(self))
+
     def pt_restant(self): # renvoie le nombre de point que totalise les dominos de la main, il s'agit de minimiser ce total
         pt_total = 0
         for domino in self :
             pt_total += domino.val_totale()
         return (pt_total)
+
+    def cinq_meme_famille(self): # vérifie que la main ne contient pas 5 dominos de la même famille (possedant un nombre en commun)
+        count_control = [0] * (self.game.pt_max + 1)
+        for domino in self : # un domino appartient à deux famille si val_a et val_b sont différent, à une unique famille si val_a et val_b sont égaux
+            if domino.vala == domino.valb :
+                count_control[domino.vala] += 1
+            else :
+                count_control[domino.vala] += 1
+                count_control[domino.valb] += 1
+        for card_famille in count_control : # on controle le cardinal de chaque famille de points
+            if card_famille > 5 :
+                return(True)
+        return(False)
+
 
 
 
@@ -117,7 +133,7 @@ class plateau(list):
                 self.insert(0,domino.inverted())
                 self.extr_a = domino.valb
 
-        if extr == self.extr_b :
+        elif extr == self.extr_b :
             if domino.vala == self.extr_b :
                 self.append(domino)
                 self.extr_b = domino.valb
@@ -131,23 +147,25 @@ class game:
 
     def __init__(self,pt_max = 6,nb_joueur = 2,nb_dominoparjoueur = 7):
         '''pt_max est le nombre de points maximal sur un domino'''
+
         self.nb_joueur = nb_joueur
         self.pt_max = pt_max
         self.nb_domino = (pt_max+1)*(pt_max+2)/2
         self.nb_dominoparjoueur=nb_dominoparjoueur
         self.size = int(self.nb_domino * 2) # On s'assure que tous les dominos peuvent être alignés sur le plateau
-        self.plateau =  plateau(self)
+
+        self.jouer_partie()
+
+
+    def initialiser(self):
+        self.plateau = plateau(self)
         self.talon = talon(self.pt_max)
         self.Joueurs = []
-        for i in range(self.nb_joueur): # création des mains
-            self.Joueurs.append(hand(i,self))
-        self.distribuer()
-        self.Partie()
+        for i in range(self.nb_joueur):  # création des mains
+            mode = input("Donnez le mode du joueur {0} (tapez human, IA_max ou IA_hasard) ".format(i))
+            self.Joueurs.append(hand(i, self, mode))
+        print("\n")
 
-
-
-
-    def distribuer(self):
 
         for joueur in self.Joueurs:
             for i in range (self.nb_dominoparjoueur):
@@ -163,71 +181,115 @@ class game:
 
 
     def jouer_tour(self,joueur): # le joueur joue son tour
-        if self.premiere_pose :
-            max_domino = joueur.max_domino()
-            joueur.remove(max_domino)
-            self.plateau.poser(max_domino)
-            self.premiere_pose = False
+        if joueur.cinq_meme_famille() :
+            self.Jeu_en_cours = False
+            self.recommencer = True
+            print("Le joueur {0} possède 5 dominos de la même famille, il faut recommencer la partie !".format(joueur.num))
         else :
-            domino_jouable = joueur.domino_jouable()
-            if  domino_jouable == [] : # Aucun domino n'est jouable
-                if len(self.talon) > 0 : # il peut piocher donc il pioche et passe son tour
-                    domino_pioche = self.talon.tirer()
-                    joueur.append(domino_pioche)
-                    print("Joueur {0} ne peut pas jouer, il pioche {1} et passe son tour".format(joueur.num,domino_pioche))
-                else :
-                    joueur.etat_bloque = True # il ne peut pas piocher, le talon est vide, il est définitvement bloqué
-                    print("Le talon est vide : Joueur {0} ne peut définitivement plus jouer".format(joueur.num))
-            else : # le joueur peut jouer, il joue
-                print("Plateau : {0}".format(self.plateau))
-                print("Joueur {0} : {1}".format(joueur.num,joueur))
-                domino_choisit = domino_jouable[int(input("Dominos jouables : {0} /n Quel domino souhaitez vous posez ? (rang de ce domino) :".format(domino_jouable)))]
+            if self.premiere_pose:
+                max_domino = joueur.max_domino()
+                joueur.remove(max_domino)
+                self.plateau.poser(max_domino)
+                self.premiere_pose = False
+            else:
+                domino_jouable = joueur.domino_jouable()
+                if domino_jouable == []:  # Aucun domino n'est jouable
+                    if len(self.talon) > 0:  # il peut piocher donc il pioche et passe son tour
+                        domino_pioche = self.talon.tirer()
+                        joueur.append(domino_pioche)
+                        print("Joueur {0} ne peut pas jouer, il pioche {1} et passe son tour \n".format(joueur.num,
+                                                                                                        domino_pioche))
+                    else:
+                        joueur.etat_bloque = True  # il ne peut pas piocher, le talon est vide, il est définitvement bloqué
+                        print("Le talon est vide : Joueur {0} ne peut définitivement plus jouer \n".format(joueur.num))
+                else:  # le joueur peut jouer, il joue
+                    print("Plateau : {0}".format(self.plateau))
+                    print("Joueur {0} : {1}".format(joueur.num, joueur))
+                    if joueur.mode == "human" or joueur.mode == "Human":
+                        domino_choisit = domino_jouable[int(input(
+                            "Dominos jouables : {0} \nQuel domino souhaitez vous posez ? (rang de ce domino) ".format(
+                                domino_jouable)))]
 
-                if domino_choisit in joueur.domino_jouable_left_side() :
-                    extr_choisit = self.plateau.extr_a
-                elif domino_choisit in joueur.domino_jouable_right_side() :
-                    extr_choisit = self.plateau.extr_b
-                else :
-                    extr_choisit = input("De quel coté poser ce domino ? /n Tapez gauche ou droite")
-                    if extr_choisit == "gauche" or extr_choisit == "Gauche":
-                        extr_choisit = self.plateau.extr_a
-                    elif extr_choisit == "droite" or extr_choisit == "Droite":
-                        extr_choisit = self.plateau.extr_b
+                        if domino_choisit in joueur.domino_jouable_left_side():
+                            extr_choisit = self.plateau.extr_a
+                        elif domino_choisit in joueur.domino_jouable_right_side():
+                            extr_choisit = self.plateau.extr_b
+                        else:
+                            extr_choisit = input("De quel coté poser ce domino ? Tapez gauche ou droite")
+                            if extr_choisit == "gauche" or extr_choisit == "Gauche" or extr_choisit == "0":
+                                extr_choisit = self.plateau.extr_a
+                            elif extr_choisit == "droite" or extr_choisit == "Droite" or extr_choisit == "1":
+                                extr_choisit = self.plateau.extr_b
+                    if joueur.mode == "IA_hasard":
+                        domino_choisit = domino_jouable[random.randint(0, len(domino_jouable) - 1)]
+                        if domino_choisit in joueur.domino_jouable_left_side():
+                            extr_choisit = self.plateau.extr_a
+                        elif domino_choisit in joueur.domino_jouable_right_side():
+                            extr_choisit = self.plateau.extr_b
+                        else:
+                            random_side = random.randint(0, 1)
+                            if random_side == 0:
+                                extr_choisit = self.plateau.extr_a
+                            elif random_side == 1:
+                                extr_choisit = self.plateau.extr_b
 
-                joueur.remove(domino_choisit)
-                self.plateau.poser(domino_choisit,extr_choisit)
+                    if joueur.mode == "IA_max":
+                        domino_choisit = max(domino_jouable)
+                        if domino_choisit in joueur.domino_jouable_left_side():
+                            extr_choisit = self.plateau.extr_a
+                        elif domino_choisit in joueur.domino_jouable_right_side():
+                            extr_choisit = self.plateau.extr_b
+                        else:
+                            random_side = random.randint(0, 1)
+                            if random_side == 0:
+                                extr_choisit = self.plateau.extr_a
+                            elif random_side == 1:
+                                extr_choisit = self.plateau.extr_b
 
-                if len(joueur) == 0 :
-                    self.Jeu_en_cours = False # le joueur à posé son dernier domino, arrêter le jeu
+                    joueur.remove(domino_choisit)
+                    self.plateau.poser(domino_choisit, extr_choisit)
+                    print("\n")
+
+                    if len(joueur) == 0:
+                        self.Jeu_en_cours = False  # le joueur à posé son dernier domino, arrêter le jeu
 
 
 
 
-    def Partie(self):
+
+
+    def jouer_partie(self):
         """La partie est gérée dans cette méthode jusqu'a la fin du jeu"""
+        self.initialiser() # initialisation ou remise à zéro
         self.Jeu_en_cours = True
+        self.recommencer = False
         self.premiere_pose = True
 
         while self.Jeu_en_cours :
             for joueur in self.Joueurs :
                 if self.Jeu_en_cours :
                     self.jouer_tour(joueur)
-            if [joueur.etat_bloque for joueur in self.Joueurs] == [True]*4 :
+            if [joueur.etat_bloque for joueur in self.Joueurs] == [True]*self.nb_joueur :
                 self.Jeu_en_cours = False # Tout les joueurs sont bloqués, arrêter le jeu
 
         self.fin_de_partie()
 
     def fin_de_partie(self):
-        gagnant = self.Joueurs[0]
-        for joueur in self.Joueurs :
-            if joueur.pt_restant() < gagnant.pt_restant() :
-                gagnant = joueur
-        print("Le gagnant est : Joueur {0}".format(gagnant.num))
+        if self.recommencer :
+            print("Nouvelle Partie")
+            self.jouer_partie()
+        else :
+            gagnant = self.Joueurs[0]
+            for joueur in self.Joueurs:
+                if joueur.pt_restant() < gagnant.pt_restant():
+                    gagnant = joueur
+            print("Le gagnant est : Joueur {0}".format(gagnant.num))
 
 
 
 if __name__ == "__main__":
     Game = game()
+
 
 
 
